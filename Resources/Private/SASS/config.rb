93f9305f71aceda0e725b5fe9b45c3122d36a1d3
Søren Bryder Nielsen <soerenb@gmail.com>
2013-03-18 23:12:44 +0100

    compass setup

--- Resources/Private/SASS/config.rb
+++ Resources/Private/SASS/config.rb
@@ -1,5 +1,4 @@
 # Require any additional compass plugins here.
-<<<<<<< HEAD
 require "susy"
 
 # Set this to the root of your project when deployed:
@@ -11,12 +10,13 @@ javascripts_dir = "../Public/JavaScript"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
@@ -25,22 +25,4 @@ javascripts_dir = "../Public/JavaScript"
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 
-=======
 
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "../../Public/Stylesheets/"
-sass_dir = "src"
-images_dir = "../../Public/Images/"
-javascripts_dir = "../../Public/JavaScript/"
-
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
->>>>>>> c22fec282f6659da2c7377ecf23dd0ab829a14d1
